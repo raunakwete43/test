fb471b03e6c41b4ab0bbfb73ae29e3553951fbd0
raunakwete43 <raunakwete43@gmail.com>
2025-02-07 12:48:58 +0530

    java modified 1

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ class main {
   public static void main(String []args) {
     System.out.println("Welcome to Jenkins");
     System.out.println("Push successfull");
+    System.out.println("Automatic build on push success!");
   }
 }
